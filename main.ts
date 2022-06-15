cc419ee6b08d845f287c582884c4d6afe8c0539f
Jacob Hummer <hummerj@outlook.com>
2022-06-15 11:11:58 -0500

    Dynamically choose implementation in main() decorator

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
-// Pick on of the three isMain implementations for browser, Node, or Deno
-import isMain from "is-main"
-
 const main = (importMeta: ImportMeta) => {
 	return (function$: Function) => {
+		;(async () => {
+			let isMain: (importMeta: ImportMeta) => boolean
+			if ("window" in globalThis && "document" in globalThis) {
+				;({ default: isMain } = await import("/is-main/-.browser.ts"))
+			} else if ("process" in globalThis) {
+				;({ default: isMain } = await import("/is-main/-.node.ts"))
+			} else if ("Deno" in globalThis) {
+				;({ default: isMain } = await import("/is-main/-.deno.ts"))
+			} else {
+				// Unknown env, so just assume always NOT the main module
+				isMain = () => false
+			}
+		})()
 		if (isMain(importMeta)) {
 			// Hold off on execution to let other sync initializations take place
 			queueMicrotask(function$)
